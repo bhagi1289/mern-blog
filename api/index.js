be8761679b0c8e4615cdae5879ca15954dd62def
Bhagyaraju <bhagyaraju1289@gmail.com>
2024-01-21 19:00:12 +0530

    singup route completed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -18,9 +19,11 @@ mongoose.connect(mongoConnectionString)
 
 const app = express();
 
+app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT} !!!`)
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
